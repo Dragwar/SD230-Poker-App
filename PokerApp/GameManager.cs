62625883c7c68ab712f65ad6cc9bcacbc82b122c
Everett Grassler <dragwar9@gmail.com>
2019-03-22 13:02:46 -0500

    Added constants for list limits

--- PokerApp/GameManager.cs
+++ PokerApp/GameManager.cs
@@ -10,6 +10,11 @@ namespace PokerApp
         private Random Random { get; }
         private List<IPlayer> _Players { get; set; }
         private DeckManager DeckManager { get; }
+
+        private const int PlayerHandSize = 5;
+        private const int MaxPlayerLimit = 8;
+        private const int MinPlayerLimit = 2;
+
         internal IReadOnlyList<IPlayer> Players { get => _Players.ToList().AsReadOnly(); }
         internal IReadOnlyList<PlayingCard> Deck { get => DeckManager.Deck.AsReadOnly(); }
 
@@ -28,11 +33,11 @@ namespace PokerApp
         }
         private List<IPlayer> CheckValidPlayerCount(List<IPlayer> players)
         {
-            if (players.Count < 2)
+            if (players.Count < MinPlayerLimit)
             {
                 throw new ArgumentOutOfRangeException();
             }
-            else if (players.Count > 8)
+            else if (players.Count > MaxPlayerLimit)
             {
                 throw new ArgumentOutOfRangeException();
             }
@@ -46,7 +51,7 @@ namespace PokerApp
         {
             foreach (IPlayer player in _Players)
             {
-                for (int i = 0; i < 5; i++)
+                for (int i = 0; i < PlayerHandSize; i++)
                 {
                     PlayingCard dealtCard = DeckManager.Deal();
                     player.Hand.Cards.Add(dealtCard);
