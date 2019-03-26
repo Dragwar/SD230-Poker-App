bb894f8082add240cc49e157d420cdb29063c582
Everett Grassler <dragwar9@gmail.com>
2019-03-26 00:26:57 -0500

    Got rid of unused file and this app now finds all ranks

--- PokerApp/Hand.cs
+++ PokerApp/Hand.cs
@@ -17,30 +17,30 @@ namespace PokerApp
             Cards = cards;
         }
 
+        //TODO: Make rank checks more modular or separate them into methods
         private HandRankEnum DetermineRank()
         {
             // just for testing hands
-            //List<PlayingCard> testingHand = new List<PlayingCard>()
-            //{
-            //    new PlayingCard(CardNameValueEnum.Ace, SuitEnum.Clubs),
-            //    new PlayingCard(CardNameValueEnum.Two, SuitEnum.Clubs),
-            //    new PlayingCard(CardNameValueEnum.Ten, SuitEnum.Clubs),
-            //    new PlayingCard(CardNameValueEnum.Queen, SuitEnum.Clubs),
-            //    new PlayingCard(CardNameValueEnum.Eight, SuitEnum.Clubs),
-            //};
+            List<PlayingCard> testingHand = new List<PlayingCard>()
+            {
+                new PlayingCard(CardNameValueEnum.Ace, SuitEnum.Diamonds),
+                new PlayingCard(CardNameValueEnum.Two, SuitEnum.Diamonds),
+                new PlayingCard(CardNameValueEnum.Three, SuitEnum.Diamonds),
+                new PlayingCard(CardNameValueEnum.Four, SuitEnum.Diamonds),
+                new PlayingCard(CardNameValueEnum.Five, SuitEnum.Diamonds),
+            };
 
-            // all ranks the hand falls under
-            List<HandRankEnum> tempRanks = new List<HandRankEnum>();
-
-
-            //List<FullCardInfo> cards = GetCardCountNameValueList(testingHand);
+            //List<FullCardInfo> cards = GetListOfFullCardInfo(testingHand);
             List<FullCardInfo> cards = GetListOfFullCardInfo(Cards);
+
+            // all ranks the current hand falls under
+            List<HandRankEnum> tempRanks = new List<HandRankEnum>();
 
             // (int Card Count, FullCardInfo Card)
             List<IGrouping<int, FullCardInfo>> cardGroupPairs = cards.GroupBy(card => card.CardCount).ToList();
 
 
-            // Find pairs and XOfKinds
+            // Find pairs and {number}OfKinds
             foreach (IGrouping<int, FullCardInfo> item in cardGroupPairs)
             {
                 switch (item.Key)
@@ -57,46 +57,75 @@ namespace PokerApp
                         tempRanks.Add(HandRankEnum.OnePair);
                         break;
 
-                    // is hand ThreeOfAKind
+                    // does hand contain a ThreeOfAKind
                     case 3: tempRanks.Add(HandRankEnum.ThreeOfAKind); break;
 
-                    // is hand FourOfAKind
+                    // does hand contain a FourOfAKind
                     case 4: tempRanks.Add(HandRankEnum.FourOfAKind); break;
 
-                    // is hand FiveOfAKind
-                    case 5: tempRanks.Add(HandRankEnum.FiveOfAKind); break;
+                    // is hand FiveOfAKind (skip checking anything else because this is the highest rank)
+                    case 5: tempRanks.Add(HandRankEnum.FiveOfAKind); return HandRankEnum.FiveOfAKind;
                 }
             }
 
 
-            // is hand FullHouse?
-            if (tempRanks.Contains(HandRankEnum.ThreeOfAKind) && tempRanks.Count(rank => rank == HandRankEnum.OnePair) == 1)
+            // skip these checks if hand is already a FourOfAKind (skip unnecessary rank checks)
+            if (!tempRanks.Contains(HandRankEnum.FourOfAKind))
             {
-                tempRanks.Add(HandRankEnum.FullHouse);
-            }
-
-            // is hand Flush?
-            foreach (int suit in Enum.GetValues(typeof(SuitEnum)))
-            {
-                bool isFlush = cards.All(card => card.Suit == (SuitEnum)suit);
-                if (isFlush)
+                // is hand FullHouse?
+                if (tempRanks.Contains(HandRankEnum.ThreeOfAKind) && tempRanks.Count(rank => rank == HandRankEnum.OnePair) == 1)
                 {
-                    tempRanks.Add(HandRankEnum.Flush);
+                    tempRanks.Add(HandRankEnum.FullHouse);
+                }
+
+                // skip these checks if hand is already a FullHouse (skip unnecessary rank checks)
+                if (!tempRanks.Contains(HandRankEnum.FullHouse))
+                {
+                    // is hand Flush?
+                    foreach (int suit in Enum.GetValues(typeof(SuitEnum)))
+                    {
+                        bool isFlush = cards.All(card => card.Suit == (SuitEnum)suit);
+                        if (isFlush)
+                        {
+                            tempRanks.Add(HandRankEnum.Flush);
+                        }
+                    }
+
+
+                    // is hand Straight?
+                    bool isStraight = true;
+                    int minValue = cards.Min(card => card.Value);
+                    for (int i = 0; i < 5; i++)
+                    {
+                        //re-factor (maybe): minValue++ could be moved below this if for more readability
+                        if (cards[i].Value != minValue++)
+                        {
+                            isStraight = false;
+                            break;
+                        }
+                    }
+
+                    if (isStraight)
+                    {
+                        tempRanks.Add(HandRankEnum.Straight);
+                    }
                 }
             }
 
-            //TODO: figure out (Straight)
 
-
-            //TODO: figure out (StraightFlush)
-
+            // is hand a StraightFlush?
+            if (tempRanks.Contains(HandRankEnum.Straight) && tempRanks.Contains(HandRankEnum.Flush))
+            {
+                tempRanks.Add(HandRankEnum.StraightFlush);
+            }
 
             // Default to HighCard if no rank was added
             HandRankEnum handRank = tempRanks.Any() ? tempRanks.Max() : HandRankEnum.HighCard;
+
             return handRank;
         }
 
-        public List<FullCardInfo> GetListOfFullCardInfo(List<PlayingCard> cards)
+        internal List<FullCardInfo> GetListOfFullCardInfo(List<PlayingCard> cards)
         {
             //NOTE: Remember if hand is one pair then the count of cardCountNameValues will be 4 (won't change count depending on pairs)
             List<CardCountNameValue> cardCountNameValues = cards.GroupBy(card => card.Value)
