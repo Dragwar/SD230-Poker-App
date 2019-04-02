eb6d5e1b3f09c59ae8b7cf43d6d0abce241015db
Everett Grassler <dragwar9@gmail.com>
2019-04-01 21:38:22 -0500

    Refactored program.cs
    - Removed goto statements
    - Separated code into more methods

--- PokerApp/Program.cs
+++ PokerApp/Program.cs
@@ -6,21 +6,28 @@ namespace PokerApp
     {
         public static void Main()
         {
-            //TODO: re-factor this method by splitting it into multiple methods
-            string userName;
-            do
-            {
-                Console.WriteLine("Please provide a name");
-                userName = Console.ReadLine().Trim();
-            } while (string.IsNullOrWhiteSpace(userName));
-
-
-
-        GameStart:
-            IPlayer mainPlayer = new Player(userName);
-            GameManager game = new GameManager(mainPlayer, 2);
-
             Console.Title = $"Deck consists of: (52 total playing cards), (4 suits), (13 playing cards per suit)";
+
+            (string userName, int? numberOfAI, bool? isStrictDraw) = SetUpGame();
+
+            Start((userName, numberOfAI, isStrictDraw));
+        }
+
+        private static void Start((string userName, int? numberOfAI, bool? isStrictDraw)? data)
+        {
+            // if invalid data, then require data
+            if (!data.HasValue || 
+                string.IsNullOrWhiteSpace(data.Value.userName) || 
+                !data.Value.isStrictDraw.HasValue ||
+                !data.Value.numberOfAI.HasValue ||
+                data.Value.numberOfAI < 1 || 
+                data.Value.numberOfAI > 7)
+            {
+                data = SetUpGame();
+            }
+
+            IPlayer mainPlayer = new Player(data.Value.userName);
+            GameManager game = new GameManager(mainPlayer, data.Value.numberOfAI.Value);
 
 
             Console.WriteLine("Now Dealing Five Cards To Each Player...");
@@ -33,28 +40,82 @@ namespace PokerApp
 
             Console.WriteLine("\nPlayer Hands:");
             game.DisplayHandInfo(true);
-            game.CheckWinConditions(true);
+            game.CheckWinConditions(data.Value.isStrictDraw.Value);
 
 
+            CheckToRestartGame((data.Value.userName, data.Value.numberOfAI.Value, data.Value.isStrictDraw));
+        }
 
-        LeavingMenu:
-            Console.WriteLine("\nPress 1 to Restart Test");
-            Console.WriteLine("Press 2 to Exit");
-            ConsoleKey key = Console.ReadKey(true).Key;
-
-            switch (key)
+        private static (string, int?, bool?) SetUpGame()
+        {
+            // Player name
+            string userName;
+            do
             {
-                case ConsoleKey.D1:
-                    Console.Clear();
-                    Console.WriteLine("Restarting . . .\n");
-                    goto GameStart;
+                Console.WriteLine();
+                Console.WriteLine("Please provide a name");
+                userName = Console.ReadLine().Trim();
+            } while (string.IsNullOrWhiteSpace(userName));
 
-                case ConsoleKey.D2:
-                    break;
 
-                default:
-                    goto LeavingMenu;
-            }
+            // Number of players
+            int num;
+            int? numberOfAI;
+            bool isNumber = false;
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Number of AI players (between 1-7) [excluding yourself]");
+                isNumber = int.TryParse(Console.ReadLine().Trim(), out num);
+                numberOfAI = num;
+            } while (!isNumber || numberOfAI.Value < 1 || numberOfAI.Value > 7);
+
+
+            // Draw rules
+            bool? isStrictDraw = null;
+            ConsoleKey key;
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Tie/Draw rules:");
+                Console.WriteLine("[Strict-Draw] --> (Y) - Only players with the same Hand Rank and same HighCard Value will be a tie ");
+                Console.WriteLine("[Non-Strict-Draw] --> (N) - Player's with the same hand will always be a tie/draw regardless of the HighCard");
+                key = Console.ReadKey(true).Key;
+                isStrictDraw = key == ConsoleKey.Y ? true : key == ConsoleKey.N ? false : isStrictDraw;
+            } while ((key != ConsoleKey.Y || key != ConsoleKey.N) && !isStrictDraw.HasValue);
+
+            return (userName, numberOfAI.Value, isStrictDraw.Value);
+        }
+
+        private static void CheckToRestartGame((string userName, int? numberOfAI, bool? isStrictDraw) gameData)
+        {
+            bool isValidKey = false;
+            do
+            {
+                Console.WriteLine("\nPress 1 to Restart Test (with same settings)");
+                Console.WriteLine("Press 2 to Restart Test (Full Reset)");
+                Console.WriteLine("Press 3 to Exit");
+                ConsoleKey key = Console.ReadKey(true).Key;
+
+                switch (key)
+                {
+                    case ConsoleKey.D1:
+                        isValidKey = true;
+                        Console.Clear();
+                        Console.WriteLine("Restarting . . .\n");
+                        Start(gameData);
+                        break;
+
+                    case ConsoleKey.D2:
+                        isValidKey = true;
+                        Start(null);
+                        break;
+
+                    case ConsoleKey.D3: isValidKey = true; break;
+
+                    default: break;
+                }
+            } while (!isValidKey);
         }
     }
 }
